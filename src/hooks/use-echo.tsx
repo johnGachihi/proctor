d4ba203a2926d746fbec7fa532adfc6dbe064205
johnGachihi <johngachihi3@gmail.com>
2020-10-25 15:40:23 +0300

    Add base useEcho implementation
    Not yet used it to build the exposed hooks (useEchoPrivate or
    useEchoPresence).

--- src/hooks/use-echo.tsx
+++ src/hooks/use-echo.tsx
@@ -1,7 +1,8 @@
+import { Channel } from "laravel-echo/dist/channel";
 import { useCallback, useEffect, useState } from "react";
 import EchoClient from '../network/echo-client'
 
-
+// TODO: Use useEcho to build this
 function useEchoPresence(channel: string) {
   const [subscribers, setSubscribers] = useState([])
   const [presenceChannel, setPresenceChannel] = useState<any>()
@@ -37,6 +38,7 @@ function useEchoPresence(channel: string) {
   return { subscribers, listen, stopListening };
 }
 
+// TODO: Use useEcho to build this
 function useEchoPrivate(channel: string) {
   const [privateChannel, setPrivateChannel] = useState<any>()
   
@@ -61,6 +63,47 @@ function useEchoPrivate(channel: string) {
       privateChannel.stopListening(event)
     }
   }, [privateChannel])
+
+  return { listen, stopListening }
+}
+
+// useEcho usage example
+/* function useEchoExample(channelName: string) {
+  const connect = useCallback(() => {
+    return EchoClient.private(channelName)
+  }, [channelName])
+
+  const leave = useCallback(() => {
+    return EchoClient.leave(channelName)
+  }, [channelName])
+
+  return useEcho(connect, leave)
+} */
+
+function useEcho(connect: () => Channel, leave: () => void) {
+  const [channel, setChannel] = useState<any>()
+  
+  useEffect(() => {
+    const channel = connect()
+    setChannel(channel)
+
+    return leave
+  }, [channel, connect, leave])
+
+  const listen = useCallback(
+    (event: string, callback: (message: any) => any) => {
+      if (channel) {
+        channel.listen(event, callback)
+      }
+    },
+    [channel]
+  )
+
+  const stopListening = useCallback((event: string) => {
+    if (channel) {
+      channel.stopListening(event)
+    }
+  }, [channel])
 
   return { listen, stopListening }
 }
