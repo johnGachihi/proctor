8e07073e957d89d01b7e7831f66cd36953332f62
johnGachihi <johngachihi3@gmail.com>
2020-10-29 12:13:35 +0300

    Use base useEcho hook to build useEchoPresence and useEchoPrivate hooks

--- src/hooks/use-echo.tsx
+++ src/hooks/use-echo.tsx
@@ -1,86 +1,37 @@
-import { Channel } from "laravel-echo/dist/channel";
+import { Channel, PresenceChannel } from "laravel-echo/dist/channel";
 import { useCallback, useEffect, useState } from "react";
 import EchoClient from '../network/echo-client'
 
-// TODO: Use useEcho to build this
+
 function useEchoPresence(channel: string) {
   const [subscribers, setSubscribers] = useState([])
-  const [presenceChannel, setPresenceChannel] = useState<any>()
 
-  useEffect(() => {
-    const presenceChannel = EchoClient.join(channel)
-      .here((users: any) => {
-        setSubscribers(users)
-      })
-      .joining(console.log)
-      .leaving(console.log)
-    
-    setPresenceChannel(presenceChannel)
-
-    return () => EchoClient.leave(channel);
-  }, [channel]);
-
-  const listen = useCallback(
-    (event: string, callback: (message: any) => any) => {
-      if (presenceChannel) {
-        presenceChannel.listen(event, callback)
-      }
-    },
-    [presenceChannel]
-  )
-
-  const stopListening = useCallback((event: string) => {
-    if (presenceChannel) {
-      presenceChannel.stopListening(event)
-    }
-  }, [presenceChannel])
-
-  return { subscribers, listen, stopListening };
-}
-
-// TODO: Use useEcho to build this
-function useEchoPrivate(channel: string) {
-  const [privateChannel, setPrivateChannel] = useState<any>()
-  
-  useEffect(() => {
-    const privateChannel = EchoClient.private(channel)
-    setPrivateChannel(privateChannel)
-
-    return () => EchoClient.leave(channel)
-  }, [channel, setPrivateChannel])
-
-  const listen = useCallback(
-    (event: string, callback: (message: any) => any) => {
-      if (privateChannel) {
-        privateChannel.listen(event, callback)
-      }
-    },
-    [privateChannel]
-  )
-
-  const stopListening = useCallback((event: string) => {
-    if (privateChannel) {
-      privateChannel.stopListening(event)
-    }
-  }, [privateChannel])
-
-  return { listen, stopListening }
-}
-
-// useEcho usage example
-/* function useEchoExample(channelName: string) {
   const connect = useCallback(() => {
-    return EchoClient.private(channelName)
-  }, [channelName])
+    return EchoClient.join(channel)
+    .here((users: any) => {
+      setSubscribers(users)
+    })
+    .joining(console.log)
+    .leaving(console.log)
+  }, [channel])
 
   const leave = useCallback(() => {
-    return EchoClient.leave(channelName)
-  }, [channelName])
+    EchoClient.leave(channel)
+  }, [channel])
+
+  const { listen, stopListening } = useEcho(connect, leave)
+
+  return {listen, stopListening, subscribers}
+}
+
+function useEchoPrivate(channel: string) {
+  const connect = useCallback(() => EchoClient.private(channel), [channel])
+  const leave = useCallback(() => EchoClient.leave(channel), [channel])
 
   return useEcho(connect, leave)
-} */
+}
 
-function useEcho(connect: () => Channel, leave: () => void) {
+function useEcho(connect: () => Channel | PresenceChannel, leave: () => void) {
   const [channel, setChannel] = useState<any>()
   
   useEffect(() => {
@@ -88,7 +39,7 @@ function useEcho(connect: () => Channel, leave: () => void) {
     setChannel(channel)
 
     return leave
-  }, [channel, connect, leave])
+  }, [connect, leave])
 
   const listen = useCallback(
     (event: string, callback: (message: any) => any) => {
